38334595a253a48ab1d1cdd3308aa9c27e55f943
Marcelo Bukowski de Farias <marcelo@codestream.com>
2021-05-13 15:39:45 -0400

    Show file paths in FR diffs

--- jb/src/main/kotlin/review/ReviewService.kt
+++ jb/src/main/kotlin/review/ReviewService.kt
@@ -11,8 +11,10 @@ import com.intellij.diff.DiffDialogHints
 import com.intellij.diff.DiffManagerEx
 import com.intellij.diff.chains.DiffRequestChain
 import com.intellij.diff.editor.DiffRequestProcessorEditor
+import com.intellij.diff.editor.DiffVirtualFile
 import com.intellij.diff.editor.SimpleDiffVirtualFile
 import com.intellij.diff.impl.CacheDiffRequestChainProcessor
+import com.intellij.diff.impl.DiffRequestProcessor
 import com.intellij.diff.requests.SimpleDiffRequest
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -67,23 +69,20 @@ class ReviewService(private val project: Project) {
                 ReviewDiffRequestProducer(project, review, repoId, it, checkpoint)
             }
 
-            diffChain = ReviewDiffRequestChain(producers).also { chain ->
+            val myDiffChain = ReviewDiffRequestChain(producers).also { chain ->
                 chain.putUserData(REVIEW_DIFF, true)
                 chain.index = producers.indexOfFirst {
                     it.repoId == repoId && it.path == path
                 }
             }
-
-            val registryValue = Registry.get("show.diff.as.editor.tab")
-            val original = registryValue.asBoolean()
+            diffChain = myDiffChain
 
             ApplicationManager.getApplication().invokeLater {
-                try {
-                    registryValue.setValue(true)
-                    DiffManagerEx.getInstance().showDiffBuiltin(project, diffChain!!, DiffDialogHints.FRAME)
-                } finally {
-                    registryValue.setValue(original)
+                val diffFile = object : DiffVirtualFile() {
+                    override fun createProcessor(project: Project): DiffRequestProcessor = CacheDiffRequestChainProcessor(project, myDiffChain)
+                    override fun getName(): String = "Feedback Request"
                 }
+                FileEditorManager.getInstance(project).openFile(diffFile, true)
             }
         }
 
@@ -195,12 +194,13 @@ class ReviewService(private val project: Project) {
                 PullRequestProducer(project, repoId, it, headSha, headBranch, baseSha, baseBranch, context)
             }
 
-            diffChain = PullRequestChain(producers).also { chain ->
+            val myDiffChain = PullRequestChain(producers).also { chain ->
                 chain.putUserData(REVIEW_DIFF, true)
                 chain.index = producers.indexOfFirst {
                     it.filePath == filePath
                 }
             }
+            diffChain = myDiffChain
 
             val registryValue = Registry.get("show.diff.as.editor.tab")
             val original = registryValue.asBoolean()
@@ -208,7 +208,7 @@ class ReviewService(private val project: Project) {
             ApplicationManager.getApplication().invokeLater {
                 try {
                     registryValue.setValue(true)
-                    DiffManagerEx.getInstance().showDiffBuiltin(project, diffChain!!, DiffDialogHints.FRAME)
+                    DiffManagerEx.getInstance().showDiffBuiltin(project, myDiffChain, DiffDialogHints.FRAME)
                 } finally {
                     registryValue.setValue(original)
                 }

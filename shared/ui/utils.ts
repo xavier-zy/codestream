b509210577b3e8bbe02d051b853d31edc41b8b34
Brian Canzanella <brian@codestream.com>
2021-05-14 13:58:56 -0400

    updated HSP fn

--- shared/ui/utils.ts
+++ shared/ui/utils.ts
@@ -520,7 +520,7 @@ export function lightOrDark(color) {
 		b = color & 255;
 	}
 
-	// HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+	// HSP (Highly Sensitive Poo) equation adapted from C code http://alienryderflex.com/hsp.html
 	hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
 
 	// Using the HSP value, determine whether the color is light or dark

6fc3f11aef0282c9195a02289855d515367d166d
Peter Pezaris <pez@codestream.com>
2021-06-07 07:23:10 -0400

    APM

---
+++ shared/ui/Authentication/NewRelicCreateAccount.tsx
@@ -0,0 +1,457 @@
+import React, { useCallback, useState } from "react";
+import cx from "classnames";
+import { CodeStreamState } from "../store";
+import { FormattedMessage } from "react-intl";
+import Icon from "../Stream/Icon";
+import Button from "../Stream/Button";
+import { Link } from "../Stream/Link";
+import {
+	goToNewUserEntry,
+	goToEmailConfirmation,
+	goToTeamCreation,
+	goToOktaConfig,
+	closePanel
+} from "../store/context/actions";
+import { TextInput } from "./TextInput";
+import { LoginResult } from "@codestream/protocols/api";
+import { RegisterUserRequestType, GetUserInfoRequestType } from "@codestream/protocols/agent";
+import { HostApi } from "../webview-api";
+import { completeSignup, startIDESignin, startSSOSignin, SignupType } from "./actions";
+import { logError } from "../logger";
+import { useDispatch, useSelector } from "react-redux";
+import { useDidMount } from "../utilities/hooks";
+import { Loading } from "../Container/Loading";
+import { isOnPrem, supportsIntegrations } from "../store/configs/reducer";
+import { Server } from "../webview-api";
+import { DialogRow } from "../Stream/Onboard";
+
+const isPasswordValid = (password: string) => password.length >= 6;
+export const isEmailValid = (email: string) => {
+	const emailRegex = new RegExp(
+		"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	);
+	return email !== "" && emailRegex.test(email);
+};
+export const isUsernameValid = (username: string) =>
+	new RegExp("^[-a-zA-Z0-9_.]{1,21}$").test(username);
+
+const isNotEmpty = s => s.length > 0;
+
+interface TeamAuthSettings {
+	limitAuthentication: boolean;
+	authenticationProviders: {
+		[id: string]: boolean;
+	};
+}
+
+interface Props {
+	email?: string;
+	teamName?: string;
+	teamId?: string;
+	inviteCode?: string;
+	type?: SignupType;
+
+	/** the following attributes are for auto-joining teams */
+	repoId?: string;
+	commitHash?: string;
+}
+
+export const NewRelicCreateAccount = (props: Props) => {
+	const dispatch = useDispatch();
+	const derivedState = useSelector((state: CodeStreamState) => {
+		return {
+			supportsIntegrations: supportsIntegrations(state.configs),
+			oktaEnabled: isOnPrem(state.configs),
+			isInVSCode: state.ide.name === "VSC",
+			supportsVSCodeGithubSignin: state.capabilities.vsCodeGithubSignin
+		};
+	});
+	const [email, setEmail] = useState(props.email || "");
+	const [emailValidity, setEmailValidity] = useState(true);
+	const [scmEmail, setScmEmail] = useState("");
+	const [username, setUsername] = useState("");
+	const [usernameValidity, setUsernameValidity] = useState(true);
+	const [password, setPassword] = useState("");
+	const [passwordValidity, setPasswordValidity] = useState(true);
+	const [fullName, setFullName] = useState("");
+	const [fullNameValidity, setFullNameValidity] = useState(true);
+	const [companyName, setCompanyName] = useState("");
+	const [companyNameValidity, setCompanyNameValidity] = useState(true);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [isInitializing, setIsInitializing] = useState(false);
+	const [unexpectedError, setUnexpectedError] = useState(false);
+	const [inviteConflict, setInviteConflict] = useState(false);
+	const [bootstrapped, setBootstrapped] = useState(true);
+	const [limitAuthentication, setLimitAuthentication] = useState(false);
+	const [authenticationProviders, setAuthenticationProviders] = useState({});
+
+	const wasInvited = props.inviteCode !== undefined;
+
+	const getUserInfo = async () => {
+		const response = await HostApi.instance.send(GetUserInfoRequestType, {});
+		// only set this if it exists, in case there is no git configured email
+		// and the user was invited, in which case we'll use props.email
+		// turn off the suggestion for now.....
+		// if (response.email) setEmail(response.email);
+		setScmEmail(response.email); // to track if they used our git-based suggestion
+		setFullName(response.name);
+		setUsername(response.username);
+		setBootstrapped(true);
+	};
+
+	const getTeamAuthInfo = async teamId => {
+		setIsInitializing(true);
+		try {
+			const url = `/no-auth/teams/${teamId}/auth-settings`;
+			// console.warn("Request: ", url);
+			const response = await Server.get<TeamAuthSettings>(url);
+			if (response && response.limitAuthentication) {
+				setLimitAuthentication(true);
+				setAuthenticationProviders(response.authenticationProviders);
+			}
+			// console.warn("Response: ", response);
+		} catch (e) {
+			console.warn("Error in getTeamAuthInfo: ", e);
+		}
+		setIsInitializing(false);
+	};
+
+	useDidMount(() => {
+		getUserInfo();
+		if (props.teamId) getTeamAuthInfo(props.teamId);
+	});
+
+	const onValidityChanged = useCallback((field: string, validity: boolean) => {
+		switch (field) {
+			case "email": {
+				setEmailValidity(validity);
+				break;
+			}
+			case "username":
+				setUsernameValidity(validity);
+				break;
+			case "password":
+				setPasswordValidity(validity);
+				break;
+			case "fullName":
+				setFullNameValidity(validity);
+				break;
+			case "companyName":
+				setCompanyNameValidity(validity);
+				break;
+			default: {
+			}
+		}
+	}, []);
+
+	const onSubmit = async (event: React.SyntheticEvent) => {
+		setInviteConflict(false);
+		setUnexpectedError(false);
+		event.preventDefault();
+		if (isSubmitting) return; // prevent double-clicks
+
+		onValidityChanged("email", isEmailValid(email));
+		onValidityChanged("password", isPasswordValid(password));
+		onValidityChanged("username", isUsernameValid(username));
+		onValidityChanged("fullName", isNotEmpty(fullName));
+		onValidityChanged("companyName", isNotEmpty(companyName));
+
+		if (
+			email === "" ||
+			!emailValidity ||
+			!usernameValidity ||
+			password === "" ||
+			!passwordValidity ||
+			fullName === "" ||
+			!fullNameValidity
+			// (!wasInvited && (companyName === "" || !companyNameValidity))
+		)
+			return;
+		setIsSubmitting(true);
+		try {
+			const attributes = {
+				email,
+				username,
+				password,
+				fullName,
+				inviteCode: props.inviteCode,
+				companyName: wasInvited ? undefined : companyName,
+
+				// for auto-joining teams
+				commitHash: props.commitHash,
+				repoId: props.repoId,
+				teamId: props.commitHash ? props.teamId : undefined
+			};
+			const { status, token } = await HostApi.instance.send(RegisterUserRequestType, attributes);
+
+			const sendTelemetry = () => {
+				HostApi.instance.track("Account Created", {
+					email: email,
+					"Git Email Match?": email === scmEmail,
+					"Changed Invite Email?": wasInvited ? email !== props.email : undefined
+				});
+			};
+
+			switch (status) {
+				case LoginResult.Success: {
+					sendTelemetry();
+					dispatch(
+						goToEmailConfirmation({
+							email: attributes.email,
+							teamId: props.teamId,
+							registrationParams: attributes
+						})
+					);
+					break;
+				}
+				case LoginResult.NotOnTeam: {
+					sendTelemetry();
+					dispatch(goToTeamCreation({ token, email: attributes.email }));
+					break;
+				}
+				case LoginResult.AlreadyConfirmed: {
+					// because user was invited
+					sendTelemetry();
+					dispatch(
+						completeSignup(attributes.email, token!, props.teamId!, {
+							createdTeam: false
+						})
+					);
+					break;
+				}
+				case LoginResult.InviteConflict: {
+					setInviteConflict(true);
+					setIsSubmitting(false);
+					break;
+				}
+				default:
+					throw status;
+			}
+		} catch (error) {
+			logError(`Unexpected error during registration request: ${error}`, {
+				email,
+				inviteCode: props.inviteCode
+			});
+			setUnexpectedError(true);
+			setIsSubmitting(false);
+		}
+	};
+
+	const onClickGoBack = useCallback(
+		(event: React.SyntheticEvent) => {
+			event.preventDefault();
+			return dispatch(closePanel());
+		},
+		[props.type]
+	);
+
+	const buildSignupInfo = () => {
+		const info: any = { fronSignup: true };
+
+		if (props.inviteCode) {
+			info.type = SignupType.JoinTeam;
+			info.inviteCode = props.inviteCode;
+		} else if (props.commitHash) {
+			info.type = SignupType.JoinTeam;
+			info.repoInfo = {
+				teamId: props.teamId,
+				commitHash: props.commitHash,
+				repoId: props.repoId
+			};
+		} else {
+			info.type = SignupType.CreateTeam;
+		}
+
+		info.fromSignup = true;
+
+		return info;
+	};
+
+	const onClickGithubSignup = useCallback(
+		(event: React.SyntheticEvent) => {
+			event.preventDefault();
+			HostApi.instance.track("Provider Auth Selected", {
+				Provider: "GitHub"
+			});
+			if (derivedState.isInVSCode && derivedState.supportsVSCodeGithubSignin) {
+				return dispatch(startIDESignin("github", buildSignupInfo()));
+			} else {
+				return dispatch(startSSOSignin("github", buildSignupInfo()));
+			}
+		},
+		[props.type]
+	);
+
+	const onClickGitlabSignup = useCallback(
+		(event: React.SyntheticEvent) => {
+			event.preventDefault();
+			HostApi.instance.track("Provider Auth Selected", {
+				Provider: "GitLab"
+			});
+			return dispatch(startSSOSignin("gitlab", buildSignupInfo()));
+		},
+		[props.type]
+	);
+
+	const onClickBitbucketSignup = useCallback(
+		(event: React.SyntheticEvent) => {
+			event.preventDefault();
+			HostApi.instance.track("Provider Auth Selected", {
+				Provider: "Bitbucket"
+			});
+			return dispatch(startSSOSignin("bitbucket", buildSignupInfo()));
+		},
+		[props.type]
+	);
+
+	const onClickOktaSignup = useCallback(
+		(event: React.SyntheticEvent) => {
+			return dispatch(goToOktaConfig({ fromSignup: true, inviteCode: props.inviteCode }));
+		},
+		[props.type]
+	);
+
+	if (!bootstrapped || isInitializing) return <Loading />;
+
+	const showOr =
+		!limitAuthentication ||
+		(authenticationProviders["email"] &&
+			(authenticationProviders["github*com"] ||
+				authenticationProviders["gitlab*com"] ||
+				authenticationProviders["bitbucket*org"]));
+	const showOauth =
+		!limitAuthentication ||
+		authenticationProviders["github*com"] ||
+		authenticationProviders["gitlab*com"] ||
+		authenticationProviders["bitbucket*org"];
+
+	return (
+		<div className="onboarding-page">
+			<form className="standard-form" onSubmit={onSubmit}>
+				<fieldset className="form-body" style={{ paddingTop: 0, paddingBottom: 0 }}>
+					{(!limitAuthentication || authenticationProviders["email"]) && (
+						<div className="border-bottom-box" style={{ maxWidth: "none" }}>
+							<h3>Free access to all of New Relic. Forever.</h3>
+							<p className="explainer">No credit card required.</p>
+							<div className="small-spacer" />
+							<div id="controls">
+								{unexpectedError && (
+									<div className="error-message form-error">
+										<FormattedMessage
+											id="error.unexpected"
+											defaultMessage="Something went wrong! Please try again, or "
+										/>
+										<FormattedMessage id="contactSupport" defaultMessage="contact support">
+											{text => <Link href="https://help.codestream.com">{text}</Link>}
+										</FormattedMessage>
+										.
+									</div>
+								)}
+								<div className="control-group">
+									<label>
+										<FormattedMessage id="signUp.fullName.label" />
+									</label>
+									<TextInput
+										name="fullName"
+										value={fullName}
+										onChange={setFullName}
+										required
+										validate={isNotEmpty}
+										onValidityChanged={onValidityChanged}
+										autoFocus
+									/>
+									{!fullNameValidity && <small className="explainer error-message">Required</small>}
+								</div>
+								<div className="control-group">
+									<label>Work Email</label>
+									<TextInput
+										name="email"
+										value={email}
+										onChange={setEmail}
+										onValidityChanged={onValidityChanged}
+										validate={isEmailValid}
+										required
+									/>
+									{!emailValidity && (
+										<small className="explainer error-message">
+											<FormattedMessage id="signUp.email.invalid" />
+										</small>
+									)}
+								</div>
+
+								<Button className="row-button" onClick={onSubmit} loading={isSubmitting}>
+									<Icon name="newrelic" />
+									<div className="copy">Start now</div>
+									<Icon name="chevron-right" />
+								</Button>
+							</div>
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<h3> With your new account, you get:</h3>
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<DialogRow>
+								<Icon name="check" />
+								<div>
+									<b style={{ color: "var(--text-color-highlight" }}>Perpetually free access</b>
+									<br />
+									100 GB/month of free data ingest. 1 free full access user. Unlimited free basic
+									users.
+								</div>
+							</DialogRow>
+							<DialogRow>
+								<Icon name="check" />
+								<div>
+									<b style={{ color: "var(--text-color-highlight" }}>
+										One data platform for all metrics, logs, events, and traces
+									</b>
+									<br />
+									Petabyte scale. Millisecond speed. Pennies per gigabyte (beyond free tier).
+								</div>
+							</DialogRow>
+							<DialogRow>
+								<Icon name="check" />
+								<div>
+									<b style={{ color: "var(--text-color-highlight" }}>
+										Easily visualize, analyze, &amp; troubleshoot your entire stack
+									</b>
+									<br />
+									APM, Infrastructure Monitoring, Digital Experience Monitoring, Applied
+									Intelligence, and more.
+								</div>
+							</DialogRow>
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+							<div className="small-spacer" />
+						</div>
+					)}
+					<div id="controls">
+						<div className="footer">
+							<small className="fine-print">
+								<FormattedMessage id="signUp.legal.start" />{" "}
+								<FormattedMessage id="signUp.legal.terms">
+									{text => <Link href="https://codestream.com/terms">{text}</Link>}
+								</FormattedMessage>{" "}
+								<FormattedMessage id="and" />{" "}
+								<FormattedMessage id="signUp.legal.privacyPolicy">
+									{text => <Link href="https://codestream.com/privacy">{text}</Link>}
+								</FormattedMessage>
+							</small>
+							<Link onClick={onClickGoBack}>
+								<p>{"< Back"}</p>
+							</Link>
+						</div>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	);
+};
---
+++ shared/ui/Stream/Commits.tsx
@@ -0,0 +1,193 @@
+import { DEFAULT_FR_QUERIES } from "@codestream/webview/store/preferences/reducer";
+import { setUserPreference } from "@codestream/webview/Stream/actions";
+import React from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import * as reviewSelectors from "../store/reviews/reducer";
+import * as userSelectors from "../store/users/reducer";
+import { CodeStreamState } from "../store";
+import { Row } from "./CrossPostIssueControls/IssueDropdown";
+import Icon from "./Icon";
+import { Headshot } from "../src/components/Headshot";
+import {
+	setCurrentReview,
+	setNewPostEntry,
+	openPanel,
+	openModal,
+	setCreatePullRequest
+} from "../store/context/actions";
+import { useDidMount } from "../utilities/hooks";
+import { bootstrapReviews } from "../store/reviews/actions";
+import Tooltip from "./Tooltip";
+import Timestamp from "./Timestamp";
+import { ReposScm } from "@codestream/protocols/agent";
+import Tag from "./Tag";
+import {
+	PaneHeader,
+	PaneBody,
+	NoContent,
+	PaneState,
+	PaneNode,
+	PaneNodeName
+} from "../src/components/Pane";
+import { WebviewModals, WebviewPanels } from "../ipc/webview.protocol.common";
+import { Link } from "./Link";
+
+interface Props {
+	openRepos: ReposScm[];
+	paneState: PaneState;
+}
+
+export const Commits = React.memo(function Commits(props: Props) {
+	const dispatch = useDispatch();
+	const derivedState = useSelector((state: CodeStreamState) => {
+		const { session, preferences } = state;
+
+		const queries = preferences.fetchRequestQueries || DEFAULT_FR_QUERIES;
+
+		const currentUserId = session.userId!;
+		const teamMembers = userSelectors.getTeamMembers(state);
+		const feedbackRequests = queries.map(_ => {
+			const reviews = reviewSelectors.getByStatusAndUser(state, _.query, currentUserId);
+			return _.limit ? reviews.slice(0, _.limit) : reviews;
+		});
+
+		return {
+			team: state.teams[state.context.currentTeamId],
+			teamTagsHash: userSelectors.getTeamTagsHash(state),
+			queries,
+			feedbackRequests,
+			currentUserId,
+			teamMembers
+		};
+	}, shallowEqual);
+
+	const bootstrapped = useSelector((state: CodeStreamState) => state.reviews.bootstrapped);
+
+	const setQueries = queries => {
+		dispatch(setUserPreference(["fetchRequestQueries"], [...queries]));
+	};
+
+	useDidMount(() => {
+		if (!bootstrapped) {
+			dispatch(bootstrapReviews());
+		}
+	});
+
+	const { team, teamMembers, currentUserId, feedbackRequests, queries } = derivedState;
+	const { adminIds } = team;
+
+	const sortedFeedbackRequests = React.useMemo(() => {
+		return feedbackRequests.map(frRequests => {
+			const sorted = [...frRequests];
+			sorted.sort((a, b) => b.createdAt - a.createdAt);
+			return sorted;
+		});
+	}, [feedbackRequests]);
+
+	const totalFRs = React.useMemo(() => {
+		let total = 0;
+		feedbackRequests.map(_ => {
+			total += _.length;
+		});
+		return total;
+	}, [feedbackRequests]);
+
+	const toggleQueryHidden = (e, index) => {
+		if (e.target.closest(".actions")) return;
+		const newQueries = [...queries];
+		newQueries[index].hidden = !newQueries[index].hidden;
+		setQueries(newQueries);
+	};
+
+	// console.warn("Rendering reviews...");
+	return (
+		<>
+			<PaneHeader title="Commits" id={WebviewPanels.Commits} isLoading={!bootstrapped}></PaneHeader>
+			{props.paneState !== PaneState.Collapsed && (
+				<PaneBody>
+					{!bootstrapped && (
+						<Row>
+							<Icon name="sync" className="spin margin-right" />
+							<span>Loading...</span>
+						</Row>
+					)}
+					{bootstrapped && totalFRs === 0 && <NoContent>No commits found.</NoContent>}
+					{totalFRs > 0 && (
+						<>
+							{sortedFeedbackRequests.map((frRequests, index) => {
+								const query = queries[index];
+								const count = frRequests ? frRequests.length : 0;
+								return (
+									<PaneNode key={index}>
+										<PaneNodeName
+											onClick={e => toggleQueryHidden(e, index)}
+											title={query.name}
+											collapsed={query.hidden}
+											count={count}
+											isLoading={false}
+										/>
+										{!query.hidden &&
+											frRequests &&
+											frRequests.map((review, index) => {
+												const creator = teamMembers.find(user => user.id === review.creatorId);
+												return (
+													<Row
+														key={"review-" + review.id}
+														className="pane-row"
+														onClick={() => dispatch(setCurrentReview(review.id))}
+													>
+														<div>
+															<Tooltip title={creator && creator.fullName} placement="bottomLeft">
+																<span>
+																	<Headshot person={creator} />
+																</span>
+															</Tooltip>
+														</div>
+														<div>
+															<span>{review.title}</span>
+															{review.tags && review.tags.length > 0 && (
+																<span className="cs-tag-container">
+																	{(review.tags || []).map(tagId => {
+																		const tag = derivedState.teamTagsHash[tagId];
+																		return tag ? <Tag key={tagId} tag={tag} /> : null;
+																	})}
+																</span>
+															)}
+															<span className="subtle">{review.text}</span>
+														</div>
+														<div className="icons">
+															{query.query === "approved" && (
+																<Icon
+																	name="pull-request"
+																	title="Create a PR"
+																	placement="bottomLeft"
+																	delay={1}
+																	onClick={async e => {
+																		e.stopPropagation();
+																		await dispatch(setCreatePullRequest(review.id));
+																		dispatch(openPanel(WebviewPanels.NewPullRequest));
+																	}}
+																/>
+															)}
+															<Icon
+																name="review"
+																className="clickable"
+																title="Review Changes"
+																placement="bottomLeft"
+																delay={1}
+															/>
+															<Timestamp time={review.createdAt} relative abbreviated />
+														</div>
+													</Row>
+												);
+											})}
+									</PaneNode>
+								);
+							})}
+						</>
+					)}
+				</PaneBody>
+			)}
+		</>
+	);
+});

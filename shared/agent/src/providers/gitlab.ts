1ca01870ddef0812184f9949b5160d734570f7d6
pmorg <pmorg@umich.edu>
2021-06-21 14:58:31 -0400

    update gitlab queries to default only return opened issues

--- shared/agent/src/providers/gitlab.ts
+++ shared/agent/src/providers/gitlab.ts
@@ -294,7 +294,7 @@ export class GitLabProvider extends ThirdPartyIssueProviderBase<CSGitLabProvider
 	async getCards(request: FetchThirdPartyCardsRequest): Promise<FetchThirdPartyCardsResponse> {
 		await this.ensureConnected();
 
-		const url = request.customFilter ? "/issues?" + request.customFilter : "/issues";
+		const url = request.customFilter ? "/issues?" + request.customFilter : "/issues?state=opened";
 
 		try {
 			const response = await this.get<any[]>(url);

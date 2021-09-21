e141beb327f9ffa824cf05ac67b4eab4f994cc84
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2021-09-21 13:47:35 -0700

    Use Go plugin API

--- jb/src/main/kotlin/actions/PixieDynamicLoggingAction.kt
+++ jb/src/main/kotlin/actions/PixieDynamicLoggingAction.kt
@@ -3,18 +3,14 @@ package com.codestream.actions
 import com.codestream.agentService
 import com.codestream.protocols.agent.PixieDynamicLoggingFunctionParameter
 import com.codestream.protocols.agent.PixieDynamicLoggingParams
+import com.goide.psi.GoFunctionOrMethodDeclaration
+import com.goide.psi.GoMethodDeclaration
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.util.Key
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.tree.TokenSet
-import com.intellij.psi.util.elementType
-import com.intellij.psi.util.parents
+import com.intellij.psi.util.parentsOfType
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.awt.BorderLayout
@@ -23,74 +19,56 @@ import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
 
-private val PsiElement.isGoFunction: Boolean
-    get() = elementType?.debugName == "FUNCTION_DECLARATION" && elementType?.language?.id == "go"
-
 class PixieDynamicLoggingAction : DumbAwareAction() {
 
     override fun update(e: AnActionEvent) {
         val psiElement = CommonDataKeys.PSI_ELEMENT.getData(e.dataContext)
-        e.presentation.isVisible = psiElement?.isGoFunction == true
+        e.presentation.isVisible = psiElement?.parentsOfType<GoFunctionOrMethodDeclaration>(true)?.first() != null
     }
 
-    override fun actionPerformed(e: AnActionEvent) {
-        ApplicationManager.getApplication().invokeLater {
-                val psiElement = CommonDataKeys.PSI_ELEMENT.getData(e.dataContext)
-                if (psiElement?.isGoFunction == true) {
-                    val name = (psiElement.node.firstChildNode as LeafPsiElement).nextSibling.nextSibling.text
-                    val psiSignature = psiElement.children.find { it.elementType?.debugName == "SIGNATURE" }
-                    val psiParameters = psiSignature?.children?.find { it.elementType?.debugName == "PARAMETERS" }
+    override fun actionPerformed(e: AnActionEvent) = ApplicationManager.getApplication().invokeLater {
+        val psiElement = CommonDataKeys.PSI_ELEMENT.getData(e.dataContext)
+        val declaration = psiElement?.parentsOfType<GoFunctionOrMethodDeclaration>(true)?.first()
+        if (declaration !is GoFunctionOrMethodDeclaration) {
+            return@invokeLater
+        }
+        val name = declaration.name
+        val parameters = declaration.signature?.parameters?.definitionList?.map {
+            PixieDynamicLoggingFunctionParameter(it.name!!, it.elementType.debugName)
+        }
+        val receiver = (declaration as? GoMethodDeclaration)?.receiver?.name
+        val pkg = declaration.containingFile.`package`?.name
 
-                    val parameters = mutableListOf<PixieDynamicLoggingFunctionParameter>()
-                    psiParameters?.children?.forEach { p ->
-                        val type = p.children.find { c -> c.elementType?.debugName == "TYPE" }
-                        val definition = p.children.find { c -> c.elementType?.debugName == "PARAM_DEFINITION" }
-                        if (type != null && definition != null) {
-                            val parameter = PixieDynamicLoggingFunctionParameter(definition.text, type.text)
-                            parameters.add(parameter)
-                        } else {
-                            // ???
-                        }
-                    }
-
-                    val topLevelNodes = psiElement.containingFile.node.getChildren(null)
-                    val packageNode = topLevelNodes.find { it.elementType?.debugName == "PACKAGE_CLAUSE"}
-                    val packageText = packageNode?.text
-                    val packageName = packageText?.split(" ")?.get(1) ?: ""
-
-                    GlobalScope.launch {
-                        val result = e.project?.agentService?.pixieDynamicLogging(
-                            PixieDynamicLoggingParams(
-                                name,
-                                parameters,
-                                null,
-                                packageName
-                            )
-                        )
-                        val recentArgsAsString = result?.recentArgs?.joinToString(", ")
-                        val message = "Recent arguments: $recentArgsAsString"
-                        ApplicationManager.getApplication().invokeLater {
-                            SampleDialogWrapper(message).show()
-                        }
-                    }
-                } else {
-                    SampleDialogWrapper("You can't do that here").show()
-                }
+        GlobalScope.launch {
+            val result = e.project?.agentService?.pixieDynamicLogging(
+                PixieDynamicLoggingParams(
+                    name!!,
+                    parameters!!,
+                    receiver,
+                    pkg!!
+                )
+            )
+            val recentArgsAsString = result?.recentArgs?.joinToString(", ")
+            val message = "Recent arguments: $recentArgsAsString"
+            ApplicationManager.getApplication().invokeLater {
+                SampleDialogWrapper(message).show()
             }
         }
     }
 
-    class SampleDialogWrapper(val message: String) : DialogWrapper(true) {
-        override fun createCenterPanel(): JComponent? {
-            val dialogPanel = JPanel(BorderLayout())
-            val label = JLabel(message)
-            label.preferredSize = Dimension(100, 100)
-            dialogPanel.add(label, BorderLayout.CENTER)
-            return dialogPanel
-        }
+}
 
-        init {
-            title = "Pixie Dynamic Logging"
-            init()
-        }
+class SampleDialogWrapper(private val message: String) : DialogWrapper(true) {
+    override fun createCenterPanel(): JComponent {
+        val dialogPanel = JPanel(BorderLayout())
+        val label = JLabel(message)
+        label.preferredSize = Dimension(100, 100)
+        dialogPanel.add(label, BorderLayout.CENTER)
+        return dialogPanel
     }
+
+    init {
+        title = "Pixie Dynamic Logging"
+        init()
+    }
+}

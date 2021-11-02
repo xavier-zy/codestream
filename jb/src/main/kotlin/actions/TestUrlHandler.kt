d65ca3dd4b6a7566a616fc354c91b4f1cf1ec069
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2021-11-02 08:44:53 -0400

    Make test action compatible with 2021.3+

--- jb/src/main/kotlin/actions/TestUrlHandler.kt
+++ jb/src/main/kotlin/actions/TestUrlHandler.kt
@@ -14,7 +14,13 @@ class TestUrlHandler : DumbAwareAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         // JetBrainsProtocolHandler.processJetBrainsLauncherParameters("jetbrains://idea/codestream/newrelic/errorsinbox?1=1&errorGroupGuid=MXxFUlR8RVJSX0dST1VQfDMxMThlMjRkLTc3ZDMtMzJlNy05MWQyLTkyMmFjNjBiN2M0OA&traceId=7febfd9d-29ed-11ec-9f5b-0242ac11000f_1030_3622&src=NR-errorsinbox&entityId=MXxBUE18QVBQTElDQVRJT058Mjc5NzAwMw&commit=null&remote=https://source.datanerd.us/distributed-tracing/distributed_tracing_service.git&tag=null&controller=newrelic&action=errorsinbox");
-        JetBrainsProtocolHandler.processJetBrainsLauncherParameters("jetbrains://idea/codestream/newrelic/errorsinbox?1=1&errorGroupGuid=MXxFUlR8RVJSX0dST1VQfDMxMThlMjRkLTc3ZDMtMzJlNy05MWQyLTkyMmFjNjBiN2M0OA&traceId=7febfd9d-29ed-11ec-9f5b-0242ac11000f_1030_3622&src=NR-errorsinbox&entityId=MXxBUE18QVBQTElDQVRJT058Mjc5NzAwMw&commit=123c8092393aa8085320fbea2315cfe1b8d076d5&remote=https://source.datanerd.us/distributed-tracing/distributed_tracing_service.git&tag=null&controller=newrelic&action=errorsinbox");
-        JBProtocolCommand.handleCurrentCommand()
+        // JetBrainsProtocolHandler.processJetBrainsLauncherParameters("jetbrains://idea/codestream/newrelic/errorsinbox?1=1&errorGroupGuid=MXxFUlR8RVJSX0dST1VQfDMxMThlMjRkLTc3ZDMtMzJlNy05MWQyLTkyMmFjNjBiN2M0OA&traceId=7febfd9d-29ed-11ec-9f5b-0242ac11000f_1030_3622&src=NR-errorsinbox&entityId=MXxBUE18QVBQTElDQVRJT058Mjc5NzAwMw&commit=123c8092393aa8085320fbea2315cfe1b8d076d5&remote=https://source.datanerd.us/distributed-tracing/distributed_tracing_service.git&tag=null&controller=newrelic&action=errorsinbox");
+        // JetBrainsProtocolHandler.processJetBrainsLauncherParameters("jetbrains://idea/codestream/newrelic/errorsinbox?1=1&errorGroupGuid=MXxFUlR8RVJSX0dST1VQfDMxMThlMjRkLTc3ZDMtMzJlNy05MWQyLTkyMmFjNjBiN2M0OA&traceId=954c9592-36ae-11ec-9f5b-0242ac11000f_0_2443&src=NR-errorsinbox&entityId=MXxBUE18QVBQTElDQVRJT058Mjc5NzAwMw&timestamp=1635288399867&remote=git@source.datanerd.us:distributed-tracing/distributed_tracing_service.git&controller=newrelic&action=errorsinbox")
+        // JetBrainsProtocolHandler.processJetBrainsLauncherParameters("jetbrains://idea/codestream/newrelic/errorsinbox?1=1&errorGroupGuid=MXxFUlR8RVJSX0dST1VQfDMxMThlMjRkLTc3ZDMtMzJlNy05MWQyLTkyMmFjNjBiN2M0OA&traceId=954c9592-36ae-11ec-9f5b-0242ac11000f_0_2443&src=NR-errorsinbox&entityId=MXxBUE18QVBQTElDQVRJT058Mjc5NzAwMw&commit=123c8092393aa8085320fbea2315cfe1b8d076d5&timestamp=1635288399867&remote=git@source.datanerd.us:distributed-tracing/distributed_tracing_service.git&controller=newrelic&action=errorsinbox")
+        // JBProtocolCommand.handleCurrentCommand()
+
+        // 2021.3+
+        // JBProtocolCommand.execute()
+        // CommandLineProcessor.processProtocolCommand
     }
 }

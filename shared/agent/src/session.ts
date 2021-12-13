ade22ab10c684264541a734a8e088d677ac691cb
Eric Jones <etjones319@gmail.com>
2021-12-13 15:31:35 -0800

    user id to telemetry identify initial call (#787)
    * add user id to telemetry identify calls during signup flow
    
    * user id is its own argument in segement identify

--- shared/agent/src/session.ts
+++ shared/agent/src/session.ts
@@ -1329,9 +1329,11 @@ export class CodeStreamSession {
 			}
 		}
 
+		let userId = this._codestreamUserId || user.id;
+
 		const { telemetry } = Container.instance();
 		await telemetry.ready();
-		telemetry.identify(this._codestreamUserId!, props);
+		telemetry.identify(userId, props);
 		telemetry.setSuperProps(props);
 		if (user.firstSessionStartedAt !== undefined) {
 			telemetry.setFirstSessionProps(user.firstSessionStartedAt, FIRST_SESSION_TIMEOUT);

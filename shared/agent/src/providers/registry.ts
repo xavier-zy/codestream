be22488376a11acf60ec2aa5ac08140c5598ea82
Brian Canzanella <brian@codestream.com>
2021-12-17 13:27:46 -0500

    updates PR/MR timer to every 15min

--- shared/agent/src/providers/registry.ts
+++ shared/agent/src/providers/registry.ts
@@ -154,7 +154,7 @@ export class ThirdPartyProviderRegistry {
 	private _pollingInterval: NodeJS.Timer | undefined;
 
 	constructor(public readonly session: CodeStreamSession) {
-		this._pollingInterval = setInterval(this.pullRequestsStateHandler.bind(this), 12000000); // every 2 minutes
+		this._pollingInterval = setInterval(this.pullRequestsStateHandler.bind(this), 900000); // every 15 minutes
 	}
 
 	private async pullRequestsStateHandler() {

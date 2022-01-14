d027749e52bc3413955d7c2cc5a2d3f0f6d69472
Brian Canzanella <brian@codestream.com>
2022-01-14 14:35:24 -0500

    updates to node 16.13.2

--- shared/agent/sandbox/defaults.sh
+++ shared/agent/sandbox/defaults.sh
@@ -28,7 +28,7 @@ fi
 # Uncomment and setup if node is required. Available versions can be seen
 # with the command:
 #   ssh $DT_CLOUD_SERVER ls /home/web/SandboxRepos/software/node-$DT_OS_TYPE-*
-export LSPAGENT_NODE_VER=8.11.3
+export LSPAGENT_NODE_VER=16.13.2
 export PATH=$LSPAGENT_SANDBOX/node/bin:$LSPAGENT_TOP/node_modules/.bin:$PATH
 
 # Uncomment if you want to short circuit the sandbox hooks (see hooks/git_hooks.sh)

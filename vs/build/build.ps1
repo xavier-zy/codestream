0630c596566cbc715d365dcffdf07baf253b3a0c
Brian Canzanella <brian@codestream.com>
2022-01-19 17:43:20 -0500

    renames agent exe for VS

--- vs/build/build.ps1
+++ vs/build/build.ps1
@@ -96,11 +96,6 @@ function Build-AgentAndWebview {
 		throw "Creating packaged artifacts failed, ensure the agent has been built"
 	}
 
-	Copy-Item -Path ..\shared\agent\dist\agent-pkg-win-x86.exe -Destination src\CodeStream.VisualStudio\dist\agent.exe -Force
-	if ($LastExitCode -ne 0) {
-		throw "Copying packaged artifacts failed"
-	}
-
 	Write-Log "Packaging agent completed"
 
 	Write-Log "Build-AgentAndWebview completed in {$(Get-ElapsedTime($timer))}"

8805ca14f2a616f5b90abe052a8f245dbbe6f30f
David Hersh <dhersh@newrelic.com>
2022-01-25 17:50:24 -0500

    Exclude Done jira tickets

--- shared/agent/src/providers/jira.ts
+++ shared/agent/src/providers/jira.ts
@@ -340,7 +340,7 @@ export class JiraProvider extends ThirdPartyIssueProviderBase<CSJiraProviderInfo
 			let nextPage: string | undefined = `/rest/api/2/search?${qs.stringify({
 				jql:
 					request.customFilter ||
-					"assignee=currentuser() AND (status!=Closed OR resolution=Unresolved)",
+					"assignee=currentuser() AND ((status!=Closed AND status!=Done) OR resolution=Unresolved)",
 				expand: "transitions,names",
 				fields: "summary,description,updated,subtasks,status,issuetype,priority,assignee"
 			})}`;

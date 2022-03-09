126aef08e119fdab8f3952985bcaa947db33c9fd
Dillon Sellars <dsellars@newrelic.com>
2022-03-08 19:20:39 -0500

    Misc cleanup from pairing with Marcelo - Make codestream webview dynamic based on $projectDir - Add conditional gradle property for `ideDir` so you can opt-in to pointing to local IDE install (needed for m1 mac) - Make devtools only available when DEBUG set

--- jb/src/main/kotlin/actions/OpenDevTools.kt
+++ jb/src/main/kotlin/actions/OpenDevTools.kt
@@ -1,10 +1,15 @@
 package com.codestream.actions
 
+import com.codestream.DEBUG
 import com.codestream.webViewService
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 
 class OpenDevTools : DumbAwareAction() {
+    override fun update(e: AnActionEvent) {
+        e.presentation.isVisible = DEBUG
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         e.project?.webViewService?.openDevTools()
     }

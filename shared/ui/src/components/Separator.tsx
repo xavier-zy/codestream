5d7d01a23c68753867db091d242c06bd45bfa919
Colin Stryker <cstryker@newrelic.com>
2022-03-16 11:55:14 -0700

    dont offer PAT/OAuth for github in VSC

---
+++ shared/ui/src/components/Separator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Separator() {
+	return (
+		<div className="separator-label">
+			<span className="or">or</span>
+		</div>
+	);
+}
--- shared/ui/store/providers/actions.ts
+++ shared/ui/store/providers/actions.ts
@@ -35,7 +35,7 @@ export const configureAndConnectProvider = (
 	connectionLocation: ViewLocation,
 	force?: boolean
 ) => async (dispatch, getState) => {
-	const { providers, configs } = getState();
+	const { providers, configs, ide, capabilities } = getState();
 	const provider = providers[providerId];
 	const {
 		forEnterprise,
@@ -46,12 +46,13 @@ export const configureAndConnectProvider = (
 		supportsOAuthOrPAT
 	} = provider;
 	const onprem = configs.isOnPrem;
+	const isVSCGitHub = ide.name === "VSC" && name === "github" && capabilities.vsCodeGithubSignin;
 	connectionLocation = connectionLocation || "Integrations Panel";
 	if (needsConfigure || (onprem && needsConfigureForOnPrem)) {
 		dispatch(openPanel(`configure-provider-${provider.name}-${provider.id}-${connectionLocation}`));
 	} else if ((forEnterprise || isEnterprise) && name !== "jiraserver") {
 		dispatch(openPanel(`configure-enterprise-${name}-${provider.id}-${connectionLocation}`));
-	} else if (supportsOAuthOrPAT) {
+	} else if (supportsOAuthOrPAT && !isVSCGitHub) {
 		dispatch(openPanel(`oauthpat-provider-${provider.name}-${provider.id}-${connectionLocation}`));
 	} else {
 		dispatch(connectProvider(provider.id, connectionLocation, force));
